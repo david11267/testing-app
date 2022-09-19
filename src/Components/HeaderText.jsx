af581321f0ea4c3b31fc9265bd763c3ed25961e1
David Aslan <davidaslan112671@live.se>
2022-09-19 23:50:39 +0200

    useState in component fix

--- src/Components/HeaderText.jsx
+++ src/Components/HeaderText.jsx
@@ -1,16 +1,16 @@
-import React, { Component,useState } from 'react'
-import "../Css/VV.css"
-export default class HeaderText extends Component {
-  
-  render(props) {
-    return (
-      <>
+import React, { Component, useState } from "react";
+import "../Css/VV.css";
+
+function FavoriteColor() {
+  const [color, setColor] = useState("red");
+  return (
+    <div>
       <section>
-        <h1 className='VVH1'>Hello</h1>
+        <h1 className="VVH1">Hello</h1>
       </section>
 
       <section>
-        <h2>Join our program</h2>
+        <h2>Join our program {color}</h2>
         <p>Join our program and get a chance to win a free trip to the moon</p>
       </section>
 
@@ -18,8 +18,16 @@ export default class HeaderText extends Component {
         <h3>Thanks for visiting</h3>
         <p>Thanks for visiting our space program</p>
       </section>
+    </div>
+  );
+}
+
+export default class HeaderText extends Component {
+  render(props) {
+    return (
+      <>
+        <FavoriteColor />
       </>
-      
-    )
+    );
   }
 }
