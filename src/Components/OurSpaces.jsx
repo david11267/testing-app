db299e4b89e67d2fa837568f9251b6c9deb34794
David Aslan <davidaslan112671@live.se>
2022-10-03 12:01:29 +0200

    AnimatedTextHeaderStart

---
+++ src/Components/OurSpaces.jsx
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react'
+
+export default class OurSpaces extends Component {
+  render() {
+    return (
+      <section className='ourSpacesContainer'>
+        <div>
+            <p>Latest</p>
+            
+        </div>
+        
+      </section>
+    )
+  }
+}
